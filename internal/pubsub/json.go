0d9430766a688eeb980877bc33f2899df9869a86
Maciej Sieradz <macieks53@gmail.com>
2026-02-19 14:11:12 +0100

    added consumer

--- internal/pubsub/json.go
+++ internal/pubsub/json.go
@@ -3,6 +3,7 @@ package pubsub
 import (
 	"context"
 	"encoding/json"
+	"log"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
@@ -17,6 +18,39 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 		ContentType: "application/json",
 		Body:        dat,
 	})
+
+	return nil
+}
+
+func SubscribeJSON[T any](
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueueType,
+	handler func(T),
+) error {
+	channel, _, err := DeclareAndBind(conn, exchange, queueName, key, queueType)
+	if err != nil {
+		return err
+	}
+
+	ch, err := channel.Consume(queueName, "", false, false, false, false, nil)
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		for delivery := range ch {
+			var body T
+			err = json.Unmarshal(delivery.Body, &body)
+			if err != nil {
+				log.Printf("error unmarshalling body: %v", err)
+			}
+			handler(body)
+			delivery.Ack(false)
+		}
+	}()
 
 	return nil
 }
